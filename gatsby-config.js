9a9f1b1c3599b38bf9ddba3396efe88ea5917e35
casperleerink <casperleerink@gmail.com>
2020-02-16 18:10:55 -0800

    first steps learning react

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Xinyue Liu`,
+    description: `Personal Website of Videographer, Photographer and Visual Artist Xinyue Liu`,
+    author: `Casper Leerink`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -30,5 +30,6 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,47 +1,106 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+//See: https://www.gatsbyjs.org/docs/use-static-query/
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-import Header from "./header"
+import Menu from "./menu"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const widthBreakPoint = 768
+
+const LayoutDesktop = styled.div`
+  display: grid;
+  grid-gap: 0;
+  grid-template-areas:
+    'title title title title title menu'
+    'main main main main main menu';
+`
+const LayoutMobile = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: auto auto auto auto auto auto;
+  grid-template-rows: 100px auto;
+  // grid-template-areas:
+  //   'title title title title title bars'
+  //   'menu menu menu menu menu menu'
+  //   'main main main main main main';
+`
+
+
+const Title = styled.div`
+  grid-column: 1 / 6;
+  grid-row: 1 / 2;
+  color: black;
+`
+const Bars = styled.div`
+  grid-column: 6 / 7;
+  grid-row: 1 / 2;
+  cursor: pointer;
+`
+
+const Main = styled.div`
+  grid-column: 1 / 6;
+  grid-row: 2 / 3;
+`
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuExpanded: false,
+    };
+  }
+
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+        menuExpanded: !this.state.menuExpanded
+    });
+  }
+
+  render() {
+    const data = useStaticQuery(graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
+    `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+    let width = window.innerWidth;
+    if (width < widthBreakPoint) {
+      //mobile view
+      return (
+        <LayoutMobile>
+          <Title>
+            <h1>
+              <Link to="/">{data.site.siteMetadata.title}</Link>
+            </h1>
+          </Title>
+          <Bars><FontAwesomeIcon icon={faBars} size='2x' onClick={e => this.handleToggle(e)}/></Bars>
+          <Menu isMobile={true} menuExpanded={this.state.menuExpanded}/>
+          <Main>{children}</Main>
+        </LayoutMobile>
+      )
+    } else {
+      //desktop view
+      return (
+        <LayoutDesktop>
+          <Title>
+            <h1><Link to="/">{data.site.siteMetadata.title}</Link></h1>
+          </Title>
+          <Main>{children}</Main>
+          <Menu isMobile={false} /> 
+        </LayoutDesktop>
+      )
+    }
+  }
 }
 
 Layout.propTypes = {
