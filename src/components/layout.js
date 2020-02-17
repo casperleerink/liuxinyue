c8afcc76d12d29c7c0b28b640635f586c8616c84
casperleerink <casperleerink@gmail.com>
2020-02-17 13:23:16 -0800

    ange media queries

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import React from "react"
 import PropTypes from "prop-types"
 // import styled from "styled-components"
 import { Link } from "gatsby"
+import Media from "react-media"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
@@ -39,10 +40,7 @@ class Layout extends React.Component {
   //   `)
 
   render() {
-    let width = window.innerWidth;
-    if (width < widthBreakPoint) {
-      //mobile view
-      const { menuExpanded } = this.state;
+    const { menuExpanded } = this.state;
       let menu;
       if (menuExpanded) {
         //render the menu only if user clicks on bar icon
@@ -54,48 +52,50 @@ class Layout extends React.Component {
       } else {
         menu = null;
       }
-      return (
-        <div className="mobile-container" >
-          <h1 style={{
-            display: "block",
-            width: "90%",
-            float: "left"
-          }}>
-            <Link to="/">Xinyue Liu</Link>
-          </h1>
-          <FontAwesomeIcon icon= {faBars} size= '2x' onClick= {this.handleToggle} className="bars"/>
-          {menu}
-          <main style= {{
-            display:"block",
-            width: "100%",
-            float: "left",
-          }}>{this.props.children}</main>
-        </div>
-      )
-    } else {
-      //desktop view
-      return (
-        <div className="desktop-container">
-          <h1 style={{
-            display: "block",
-            width: "100%",
-            float: "left"
-          }}>
-            <Link to="/">Xinyue Liu</Link>
-          </h1>
-          <main style={{
-            display: "block",
-            width: "90%",
-            float: "left",
-          }} >{this.props.children}</main>
-          <Menu style={{
-            display: "block",
-            width: "10%",
-            float: "left",
-          }} />
-        </div>
-      )
-    }
+    return (
+      <Media queries={{ small: { maxWidth: 599 } }}>
+          {matches =>
+            matches.small ? (
+              <div className="mobile-container" >
+                <h1 style={{
+                  display: "block",
+                  width: "90%",
+                  float: "left"
+                }}>
+                  <Link to="/">Xinyue Liu</Link>
+                </h1>
+                <FontAwesomeIcon icon= {faBars} size= '2x' onClick= {this.handleToggle} className="bars"/>
+                {menu}
+                <main style= {{
+                  display:"block",
+                  width: "100%",
+                  float: "left",
+                }}>{this.props.children}</main>
+              </div>
+            ) : (
+              <div className="desktop-container">
+                <h1 style={{
+                  display: "block",
+                  width: "100%",
+                  float: "left"
+                }}>
+                  <Link to="/">Xinyue Liu</Link>
+                </h1>
+                <main style={{
+                  display: "block",
+                  width: "90%",
+                  float: "left",
+                }} >{this.props.children}</main>
+                <Menu style={{
+                  display: "block",
+                  width: "10%",
+                  float: "left",
+                }} />
+              </div>
+            )
+          }
+        </Media>
+    )
   }
 }
 
